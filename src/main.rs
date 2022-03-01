288deac2d3861c684216c08a9c7124ec6f6fa047
penguing27 <g.ingk.2927@gmail.com>
2022-03-02 00:04:38 +0900

    Accept command input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("$ ");
+    let mut command = String::new();
+
+    print!("$ ");
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut command)
+        .expect("Failed to read line");
 }
