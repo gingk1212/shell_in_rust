f2a84050c951f6c8c1011b54680a2d76d3433cbb
penguing27 <g.ingk.2927@gmail.com>
2022-03-02 22:55:48 +0900

    Execute command with no arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
     let mut command = String::new();
@@ -9,4 +10,13 @@ fn main() {
         .read_line(&mut command)
         .expect("Failed to read line");
     let command = command.trim();
+    invoke_cmd(command);
+}
+
+fn invoke_cmd(cmd: &str) {
+    let output = Command::new(cmd)
+        .output()
+        .expect("Command not found");
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stdout().write_all(&output.stderr).unwrap();
 }
