3a0f384f30554a1cf972834ec0dd6bc063f6f14e
penguing27 <g.ingk.2927@gmail.com>
2022-03-04 00:10:56 +0900

    Don't panic when command not found

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{self, Write};
 use std::process::Command;
+use std::error::Error;
 
 fn main() {
     loop {
@@ -12,18 +13,22 @@ fn main() {
             Ok(size) => size,
             Err(error) => panic!("Failed to read line: {:?}", error),
         };
-        invoke_cmd(&command);
+        if let Err(_) = invoke_cmd(&command) {
+            eprintln!("Command not found: {}", command.trim());
+        }
     }
 }
 
-fn invoke_cmd(cmd: &str) {
+fn invoke_cmd(cmd: &str) -> Result<(), Box<dyn Error>> {
     let mut cmd = cmd.trim().split_whitespace();
     let first_cmd = match cmd.next() {
         Some(i) => i,
-        None => return,
+        None => return Ok(()),
     };
+
     Command::new(first_cmd)
         .args(cmd)
-        .status()
-        .expect("Command not found");
+        .status()?;
+
+    Ok(())
 }
