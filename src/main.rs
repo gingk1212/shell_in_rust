480b42e29fcbce9222ad4f98675d8a9191dcce6b
penguing27 <g.ingk.2927@gmail.com>
2022-03-03 23:04:35 +0900

    Loop

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ use std::io::{self, Write};
 use std::process::Command;
 
 fn main() {
-    let mut command = String::new();
 
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    io::stdin()
-        .read_line(&mut command)
-        .expect("Failed to read line");
-    let command = command.trim();
-    invoke_cmd(command);
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line");
+        let command = command.trim();
+        invoke_cmd(command);
+    }
 }
 
 fn invoke_cmd(cmd: &str) {
