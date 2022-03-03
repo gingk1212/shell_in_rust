34abc697d52c5fcef7a245dc6209dc3fee7084c3
penguing27 <g.ingk.2927@gmail.com>
2022-03-03 23:18:22 +0900

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,12 @@ fn main() {
         io::stdin()
             .read_line(&mut command)
             .expect("Failed to read line");
-        let command = command.trim();
-        invoke_cmd(command);
+        invoke_cmd(&command);
     }
 }
 
 fn invoke_cmd(cmd: &str) {
-    let mut cmd = cmd.split_whitespace();
+    let mut cmd = cmd.trim().split_whitespace();
     let first_cmd = match cmd.next() {
         Some(i) => i,
         None => return,
