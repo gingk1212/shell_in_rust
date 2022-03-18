gingk1212/shell_in_rust

commit fcbd2e3b674e1c466986f38e734cdf7d37898bc5

Wait all commands
This commit prevents the spawning of zombie processes.

src/main.rs
@@ -60,6 +60,10 @@ fn main() {
                 eprintln!("{}", e);
             }
         }
+
+        if let Err(e) = wait_cmdline(&mut list) {
+            eprintln!("{}", e);
+        }
     }
 }
 
@@ -136,11 +140,23 @@ fn invoke_cmd(list: &mut List, from_outside: bool) -> Result<Option<&mut Child>,
         Err(e) => return Err(Box::new(e)),
     };
 
-    if is_last {
-        cmd.child.as_mut().unwrap().wait()?;
+    Ok(cmd.child.as_mut())
+}
+
+fn wait_cmdline(list: &mut List) -> Result<(), Box<dyn Error>> {
+    let mut list_now = list;
+
+    loop {
+        match list_now {
+            Cons(c, l) => {
+                c.child.as_mut().unwrap().wait()?;
+                list_now = l;
+            },
+            Nil => break,
+        }
     }
 
-    Ok(cmd.child.as_mut())
+    Ok(())
 }
 
 #[cfg(test)]

commit f4bb15d3909d7789314ab2ec04c634945fabf2d8

Avoid panic when command fails
This commit fixes a regression introduced in fcbd2e3.

src/main.rs
@@ -59,6 +59,7 @@ fn main() {
             if cfg!(debug_assertions) {
                 eprintln!("{}", e);
             }
+            continue;
         }
 
         if let Err(e) = wait_cmdline(&mut list) {